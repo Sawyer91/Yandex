e0ed16971e4f1238cde9c7220ebb900d4717eff7
Dmitriy Tyurin <t.dimon911@gmail.com>
2017-05-07 19:46:52 +0300

    Alpha 2
    Alpha 2 - new lang, new input method

--- app/src/main/java/com/example/dmitriy/yandex/MainActivity.java
+++ app/src/main/java/com/example/dmitriy/yandex/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -27,8 +29,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     TextView resultJson;
     String langJson1;
@@ -38,7 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Editable getText = null;
     Spinner spinner;
     Spinner spinner1;
-    String lang[] = {"Русский", "Английский"};
+    String lang[] = {"Русский", "Английский", "Азербайджанский", "Албанский", "Амхарский", "Арабский", "Армянский", "Белорусский", "Болгарский", "Китайский", "Корейский"};
+    ArrayList<String> res = new ArrayList<String>();
+    String js;
 
 
     @Override
@@ -46,34 +52,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //найдем View-элементы
-        resultJson = (TextView) findViewById(R.id.result_json);
-        newText = (EditText) findViewById(R.id.translate_box);
-        translateButton = (Button) findViewById(R.id.translate_button); // кнопка перевода
+        resultJson = (TextView) findViewById(R.id.result_json); // поле вывода результата
+       // resultJson = (ListView) findViewById(R.id.result_json);
+
+        newText = (EditText) findViewById(R.id.translate_box); //поле ввода
+       // translateButton = (Button) findViewById(R.id.translate_button); // кнопка перевода
         spinner = (Spinner) findViewById(R.id.lang0); //кнопка(1) выбора языка
         spinner1 = (Spinner) findViewById(R.id.lang1);//кнопка(2) выбор языка
+
+        //ArrayAdapter<String> listadapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, res);
+
 
         //Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lang);
         //  Определяем разметку для использования при выборе элемента
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        //resultJson.setAdapter(listadapter);
         spinner1.setAdapter(adapter);
         spinner.setAdapter(adapter);
-        translateButton.setOnClickListener(this); //обработчик кнопки
+        //translateButton.setOnClickListener(this); //обработчик кнопки
         spinner.setOnItemSelectedListener(itemSelectedListener1);
         spinner1.setOnItemSelectedListener(itemSelectedListener);
+
+
+        //обработчик введенных символов в TextEdit
+        newText.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                getText = newText.getText();
+                try {
+                    new ParseTask().execute();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 
     //button "перевести"
-    @Override
-    public void onClick(View v) {
-        getText = newText.getText();
-        try {
-            new ParseTask().execute();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
+   // @Override
+  //  public void onClick(View v) {
+    //    getText = newText.getText();
+   //     try {
+  //          new ParseTask().execute();
+   //    } catch (UnsupportedEncodingException e) {
+  //          e.printStackTrace();
+    //    }
+  //  }
 
     //перевод на...
     AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
@@ -84,8 +121,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Выбираем язык
             if (item.equals("Русский")) {
                 langJson1 = "ru";
-            }else {
+            }else if (item.equals("Английский")) {
                 langJson1 = "en";
+            }else if (item.equals("Азербайджанский")) {
+                langJson1 = "az";
+            }else if (item.equals("Албанский")) {
+                langJson1 = "sq";
+            }else if (item.equals("Амхарский")) {
+                langJson1 = "am";
+            }else if (item.equals("Арабский")) {
+                langJson1 = "ar";
+            }else if (item.equals("Армянский")) {
+                langJson1 = "hy";
+            }else if (item.equals("Белорусский")) {
+                langJson1 = "be";
+            }else if (item.equals("Болгарский")) {
+                langJson1 = "bg";
+            }else if (item.equals("Китайский")) {
+                langJson1 = "zh";
+            }else {
+                langJson1 = "ko";
             }
 
         }
@@ -103,8 +158,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Выбираем язык
             if (item.equals("Русский")) {
                 langJson2 = "ru";
-            }else {
+            }else if (item.equals("Английский")) {
                 langJson2 = "en";
+            }else if (item.equals("Азербайджанский")) {
+                langJson2 = "az";
+            }else if (item.equals("Албанский")) {
+                langJson2 = "sq";
+            }else if (item.equals("Амхарский")) {
+                langJson2 = "am";
+            }else if (item.equals("Арабский")) {
+                langJson2 = "ar";
+            }else if (item.equals("Армянский")) {
+                langJson2 = "hy";
+            }else if (item.equals("Белорусский")) {
+                langJson2 = "be";
+            }else if (item.equals("Болгарский")) {
+                langJson2 = "bg";
+            }else if (item.equals("Китайский")) {
+                langJson2 = "zh";
+            }else {
+                langJson2 = "ko";
             }
 
         }
@@ -145,7 +218,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 buf.close();
                 res = sb.toString();
                 return res;
-            } catch (Exception e) {     //Catch exception
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return res;
@@ -160,6 +233,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             try {
                 JSONObject jsonObject = new JSONObject(res);
                 String response = jsonObject.getString("text");
+                response = response.substring(2, response.length()-2);
                 resultJson.setText(response);
 
             } catch (JSONException e) {
@@ -168,7 +242,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
-
 
 }
 
